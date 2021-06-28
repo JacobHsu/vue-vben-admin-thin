530b991be779f5975221e4f07303c48746c374e9
jacobhsu <jacob.hsu.tw@gmail.com>
2021-06-28 16:43:15 +0800

    feat: Right Click

--- src/locales/lang/zh_CN.ts
+++ src/locales/lang/zh_CN.ts
@@ -1,13 +1,13 @@
-import { genMessage } from '../helper';
-import antdLocale from 'ant-design-vue/es/locale/zh_CN';
-import momentLocale from 'moment/dist/locale/zh-cn';
+import { genMessage } from "../helper";
+import antdLocale from "ant-design-vue/es/locale/zh_CN";
+import momentLocale from "moment/dist/locale/zh-cn";
 
-const modules = import.meta.globEager('./zh_CN/**/*.ts');
+const modules = import.meta.globEager("./zh_CN/**/*.ts");
 export default {
   message: {
-    ...genMessage(modules, 'zh_CN'),
+    ...genMessage(modules, "zh_CN"),
     antdLocale,
   },
   momentLocale,
-  momentLocaleName: 'zh-cn',
+  momentLocaleName: "zh-cn",
 };
